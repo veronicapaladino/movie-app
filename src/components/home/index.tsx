9fb605d5cc4e39b4967110c84d1dbd3ba2b2d594
Veronica Paladino <veronica.paladino@mercadolibre.com>
2023-05-20 14:43:02 -0300

    se agrega lógica de fetch movies y render en FE

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import { fetchMovies } from "../../services/movies";
+import { Movie } from "../../services/types";
+import { IMAGE_PATH } from "../../constants/movies";
 
 export const Home = () => {
-    return (
-        <>
-            <h2>Pagina principal</h2>
-        </>
-    )
-}
+  const [movies, setMovies] = useState([] as Movie[]);
+
+  useEffect(() => {
+    fetchMovies(true)
+      .then((data) => setMovies(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="container">
+      {movies.map((movie) => {
+        return (
+          <div className="container__movie" key={movie.id}>
+            <img src={`${IMAGE_PATH + movie.poster_path}`}></img>
+            <h4>{movie.title}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { API_KEY, API_URL } from "../constants/movies";
+import { Movie } from "./types";
 
-export const fetchMovies = async (searchKey: string) => {
-  const type = searchKey ? "search" : "discover";
+export const fetchMovies = async (
+  discover: boolean,
+  query = ""
+): Promise<Movie[]> => {
+  const type = discover ? "discover" : "search";
   const {
     data: { results },
   } = await axios.get(`${API_URL}/${type}/movie`, {
     params: {
-      api_key: API_KEY,
-      query: searchKey,
+      api_key: API_KEY.toString().trim(),
+      query: query,
     },
   });
   return results;
